115245bb4dac73b84d1cd8d35bfde65d3076ccfb
Timofeev Dmitriy Semenovich <DmSeTimofeev@gksm.local>
2018-03-26 13:15:52 +0300

    ejb simple

--- README.md
+++ README.md
@@ -3,17 +3,31 @@
 Сервер приложений: apache-tomee-webprofile-7.0.2
 прописать в pom адрес до сервера: папка webapps, переменная tomee.deploy.path
  настройки запуска под debug:
-
+ 
  добавить в начало
+ 
  set JPDA_ADDRESS=8000
- set JPDA_TRANSPORT=dt_socket
+  
+   set JPDA_TRANSPORT=dt_socket
 
  изменить
  call "%EXECUTABLE%" start %CMD_LINE_ARGS%
+ 
  на
  call "%EXECUTABLE%" jpda start %CMD_LINE_ARGS%
 
 
+
  Что почитать
+ 
     https://javaee.github.io/tutorial/toc.html
     https://github.com/javaee/tutorial-examples
+
+    // сервлеты
+    https://o7planning.org/ru/10395/java-servlet-filter-tutorial
+
+    //  JSP + JSTL
+    https://o7planning.org/ru/10263/java-jsp-tutorial#a38711
+    https://o7planning.org/ru/10429/java-jsp-standard-tag-library-jstl-tutorial
+    // пример JSP+JDBC
+    https://o7planning.org/ru/10285/create-a-simple-java-web-application-using-servlet-jsp-and-jdbc
--- src/main/java/ru/filters/LogFilter.java
+++ src/main/java/ru/filters/LogFilter.java
@@ -1,6 +1,10 @@
 package ru.filters;
 
+import ru.ejb.CounterBean;
+
+import javax.inject.Inject;
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Date;
@@ -8,15 +12,20 @@ import java.util.Date;
 /**
  * Servlet Filters are pluggable java components that we can use to intercept and process requests before they are sent to servlets and response
  * after servlet code is finished and before container sends the response back to the client.
- *
+ * <p>
  * Some common tasks that we can do with servlet filters are:
- *  - Logging request parameters to log files.
- *  - Authentication and autherization of request for resources.
- *  - Formatting of request body or header before sending it to servlet.
- *  - Compressing the response data sent to the client.
- *  - Alter response by adding some cookies, header information etc.
+ * - Logging request parameters to log files.
+ * - Authentication and autherization of request for resources.
+ * - Formatting of request body or header before sending it to servlet.
+ * - Compressing the response data sent to the client.
+ * - Alter response by adding some cookies, header information etc.
  */
+@WebFilter(urlPatterns = "/*")
 public class LogFilter implements Filter {
+
+    @Inject
+    private CounterBean counterBean;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -35,7 +44,8 @@ public class LogFilter implements Filter {
         System.out.println("#INFO " + new Date()
                 + " - ServletPath :" + servletPath
                 + ", URL =" + req.getRequestURL()
-                + ", Execution time (ms) = " + (System.currentTimeMillis() - startTime));
+                + ", Execution time (ms) = " + (System.currentTimeMillis() - startTime)
+                + ", req count = " + counterBean.getHits());
     }
 
     @Override
